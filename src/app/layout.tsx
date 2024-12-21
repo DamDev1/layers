a732614184a23995af687433347b4918529f8cac
DamDev1 <samueldamilare622@gmail.com>
2024-12-21 11:58:48 -0800

    Update metadata in layout.tsx: title and description changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Modern Design Tool Landing Page",
-  description: "Created with the help of Frontend Tribe",
+  title: "Modern Landing Page",
+  description: "Powered by DamDev",
 };
 
 export default function RootLayout({
