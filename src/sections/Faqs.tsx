ab4c70d58c38f6304cb6d000c113911e3a303b67
DamDev1 <samueldamilare622@gmail.com>
2024-12-21 03:19:09 -0800

    Added AnimatePresence and motion components to Faqs.tsx for animation effects

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Tag from "@/components/Tag";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
@@ -40,11 +41,15 @@ export default function Faqs() {
                         <div key={faq.question} onClick={() => setSeletedIndex(faqIndex)} className="bg-neutral-900 rounded-2xl border border-white/10 p-6 cursor-pointer">
                             <div className="flex justify-between items-center">
                                 <h3 className="font-medium">{faq.question}</h3>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={twMerge("feather feather-plus flex-shrink-0 text-lime-400", selectedIndex === faqIndex && "rotate-45")}><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={twMerge("feather feather-plus flex-shrink-0 text-lime-400 transition duration-300", selectedIndex === faqIndex && "rotate-45")}><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                             </div>
-                            <div className={twMerge("mt-6", selectedIndex!== faqIndex && "hidden")}>
-                                <p className="text-white/50">{faq.answer}</p>
-                            </div>
+                            <AnimatePresence>
+                                {selectedIndex === faqIndex && (
+                                    <motion.div initial={{ height: 0, marginTop: 0 }} animate={{ height: "auto", marginTop: 24 }} exit={{ height: 0, marginTop: 0  }} className={twMerge("overflow-hidden",)}>
+                                        <p className="text-white/50">{faq.answer}</p>
+                                    </motion.div>
+                                )}
+                            </AnimatePresence>
                         </div>
                     ))}
                 </div>
